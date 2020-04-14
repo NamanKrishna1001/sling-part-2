564ea2fec6e1a92b84cb160966b3269e46e1a4df
NamanKrishna1001 <61583807+NamanKrishna1001@users.noreply.github.com>
2020-04-14 10:03:36 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# sling-part-2
+# AngryBirdsStage3
+Stage 3 Angry Birds: Introducing Constraint
---
+++ Wood.js
@@ -0,0 +1,7 @@
+class Wood extends BaseClass {
+  constructor(x, y, width, height){
+    //super(x,y,width,height);
+    //this.image = loadImage("sprites/wood1.png");
+  }
+
+};
